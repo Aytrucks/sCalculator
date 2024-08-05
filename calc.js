dc956e4402eb61a0d3966294b73b76c3dba301d9
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-05 15:20:08 -0700

    Flex rules changed into 2 parts - Screen and Buttons

--- calc.js
+++ calc.js
@@ -1,1 +1,25 @@
+
 console.log("Script loaded");
+
+// let num1 = "";
+// let num2 = "";
+// let op = "";
+// let display = "";
+
+// //Middle row is the numpad and the function section
+// let calc = document.querySelector(".calculator");
+
+// let midRow = calc.querySelector(".middlerow");
+// console.log(midRow);
+
+// midRow.addEventListener('mouseover', (event) =>{
+//     let target = event.target;
+//     switch(target.className){
+//         case 'numpad':
+//             console.log("numpad");
+//             break;
+//         case 'functions':
+//             console.log("functions");
+//             break;
+//     }
+// });
