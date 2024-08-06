6bf12bdb7a8705dd14bca89a43502f40a38c7323
Aytrucks WDWS <105306690+Aytrucks@users.noreply.github.com>
2024-08-05 17:20:43 -0700

    Start script

--- calc.js
+++ calc.js
@@ -1,25 +1,17 @@
 
 console.log("Script loaded");
 
-// let num1 = "";
-// let num2 = "";
-// let op = "";
-// let display = "";
+let num1 = "";
+let num2 = "";
+let op = "";
+let display = "";
 
-// //Middle row is the numpad and the function section
-// let calc = document.querySelector(".calculator");
+//Middle row is the numpad functions and operations
 
-// let midRow = calc.querySelector(".middlerow");
-// console.log(midRow);
+let numpad = document.querySelector(".calculator .middlerow .numpad");
+console.log(numpad);
+let operations = document.querySelector(".calculator .middlerow .operation");
+console.log(operations);
+let functions = document.querySelector(".calculator .middlerow .functions");
+console.log(functions);
 
-// midRow.addEventListener('mouseover', (event) =>{
-//     let target = event.target;
-//     switch(target.className){
-//         case 'numpad':
-//             console.log("numpad");
-//             break;
-//         case 'functions':
-//             console.log("functions");
-//             break;
-//     }
-// });
