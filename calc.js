3b2eedc49a6dafa86b01360b3eed967da73e67dc
Aytrucks <105306690+Aytrucks@users.noreply.github.com>
2024-08-06 20:48:11 -0700

    wORK On operations

--- calc.js
+++ calc.js
@@ -8,6 +8,12 @@ let num2 = "";
 let op = "";
 let display = "";
 
+//This boolean is used to determine whether we are looking at num1 or num2 on the screen
+//It is flipped whenever an operation is clicked
+//opClick is for the numpad to change to analyzing a different number
+let n1n2 = false;
+let opClick = false;
+
 //Query select our numpad(0-9), operations(+,-,*,/), and our functions(=,AC)
 
 let numpad = document.querySelector(".calculator .middlerow .numpad");
@@ -24,6 +30,7 @@ let screen = document.querySelector(".calculator #screen");
 console.log(screen);
 
 numpad.addEventListener('click',(event) =>{
+    
     if(event.target.textContent.length === 1){
         if(screen.textContent === "NaN"){
             screen.textContent = "0";
@@ -49,6 +56,25 @@ numpad.addEventListener('click',(event) =>{
     }
 })
 
+operations.addEventListener('click',(event) =>{
+    switch(event.target.id){
+        case "plus":
+            console.log("plus");
+            num1 = screen.textContent;
+            console.log(num1);
+            break;
+        case "minus":
+            console.log("minus");
+            break;
+        case "mult":
+            console.log("mult");
+            break;
+        case "divide":
+            console.log("divide");
+            break;
+    }
+});
+
 functions.addEventListener('click',(event) =>{
     switch(event.target.id){
         case "=":
@@ -56,6 +82,7 @@ functions.addEventListener('click',(event) =>{
             break;
         case "AC":
             console.log("ac");
+            screen.textContent = "0";
             break;
     }
 });
